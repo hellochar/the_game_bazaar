4a3889e94dc49e3855cdef582322246204671fe4
James <jmuerle@jmuerle-ubuntu.(none)>
2013-03-05 18:02:54 -0800

    Locale in settings and Heroku setup

--- the_game_bazaar/settings.py
+++ the_game_bazaar/settings.py
@@ -14,7 +14,7 @@ MANAGERS = ADMINS
 import os
 
 DATABASES = {}
-if (os.environ('IS_ON_HEROKU')):
+if ('IS_ON_HEROKU' in os.environ):
     # Parse database configuration from $DATABASE_URL
     import dj_database_url
     DATABASES['default'] =  dj_database_url.config()
@@ -34,7 +34,7 @@ else:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/LosAngeles'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html

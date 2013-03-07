636b16c59937e5e33bcca1e8bb51ddafdf9d1c1d
James <jmuerle@jmuerle-ubuntu.(none)>
2013-03-06 16:27:57 -0800

    Reorganized

--- the_game_bazaar/settings.py
+++ the_game_bazaar/settings.py
@@ -115,7 +115,9 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'sdjango',
+    'the_game_bazaar',
+    'game',
+    'editor'
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:

5b1c39dc34d64a9d6fa3cca252d615f5207cc255
Fangs <fangs@ubuntu.(none)>
2013-03-09 00:35:24 -0800

    starting  to implement lobby logic

--- the_game_bazaar/urls.py
+++ the_game_bazaar/urls.py
@@ -8,10 +8,12 @@ from the_game_bazaar import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index),
-    url(r'^lobby/', include('lobby.urls', namespace="lobby")),
-    url(r'^', 'lauth.views.login_user'),
-    url(r'^list/', views.list_games),
-    url(r'^socket.io/', include('game.urls'))
+    url(r'^game/host', views.host_game),
+    url(r'^game/join', views.join_game),
+    url(r'^lobby', include('lobby.urls', namespace="lobby")),
+    # url(r'^', 'lauth.views.login_user'),
+    url(r'^list', views.list_games),
+    url(r'^socket.io', include('game.urls'))
     # Examples:
     # url(r'^$', 'the_game_bazaar.views.home', name='home'),
     # url(r'^the_game_bazaar/', include('the_game_bazaar.foo.urls')),
--- the_game_bazaar/views.py
+++ the_game_bazaar/views.py
@@ -8,3 +8,15 @@ def index(request):
 def list_games(request):
 	context = {}
 	return render(request, 'the_game_bazaar/games.html', context)
+
+def host_game(request):
+	context = {
+		'isHost': True
+	}
+	return render(request, 'the_game_bazaar/game.html', context)
+
+def join_game(request):
+	context = {
+		'isHost': False
+	}
+	return render(request, 'the_game_bazaar/game.html', context)

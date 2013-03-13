858a8e3b9e7e14d1bd3aca99aeafa7117af4cb65
hellochar <hellocharlien@hotmail.com>
2013-03-12 17:44:24 -0700

    fix views.py bad pointer

--- the_game_bazaar/views.py
+++ the_game_bazaar/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import login as auth_login
-from the_game_bazaar.models import Map
+from gmap.models import Map
 from django.views.decorators.csrf import csrf_exempt
 
 

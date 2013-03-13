0969dd4738980fcf2e6ae2e897e98ca82db81b1c
Rohan <saasbook@saasbook.(none)>
2013-03-13 16:25:01 -0700

    Added navbar to editor+cleaned up editor UI

---
+++ editor/static/editor/js/navbar.js
@@ -0,0 +1,43 @@
+$().ready(function(){
+	
+	$('#navbar-logout').submit(function (e){
+		e.preventDefault();
+		$.ajax({
+			type: "POST",
+			url: "/auth/logout/",
+			headers: {
+				"X-CSRFToken": $.cookie('csrftoken'),
+			},
+			success: function (data){
+				if(data['success'] === true){
+                    window.location.pathname = '/';
+                }
+			},
+		});
+		return false;
+	});
+
+	$('#navbar-login').submit(function (e){
+		e.preventDefault();
+		$.ajax({
+			type: "POST",
+			url: "/auth/login/",
+			data: {
+				"username":$('#navbar-username').val(),
+				"password":$('#navbar-password').val()
+			},
+			headers: {
+				"X-CSRFToken": $.cookie('csrftoken'),
+			},
+			success: function (data){
+                if(data['success'] === true){
+                    window.location.pathname = '/';
+                } else {
+                	//put stuff in error
+                }
+				return false;
+			},
+		});
+		return false;
+	});
+});

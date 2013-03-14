662dfb64533f7feacfe95279283eff94b8158048
James <jmuerle@jmuerle-ubuntu.(none)>
2013-03-14 16:06:33 -0700

    Fixed our tests and created a test bash file along with instructions in the README

--- gmap/tests.py
+++ gmap/tests.py
@@ -1,36 +1,33 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 from gmap.models import Map
 
+
 def createUser():
     User.objects.create_user("tester", "tester", "tester")
 
+
 def createTestMap():
     Map.objects.create(
-                    creator_id = "tester",
-                    num_players = 2,
-                    data = {},
-                    map_name = "qwer"
-                    )
+        creator_id=User.objects.get(username="tester"),
+        num_players=2,
+        data={},
+        map_name="qwer"
+    )
 
 
 class MapTester(TestCase):
+    def setUp(self):
+        createUser()
+        createTestMap()
+
     def test_get_map(self):
         """
         Test getting the first map
         """
-        createUser()
-        createTestMap()
-        response = self.client.get(reverse("gmap"))
-        self.assert(response.status_code, 200)
+        response = self.client.get(reverse("gmap"), {"map_id": 1})
+        self.assertEquals(response.status_code, 200)
         # fill in more tests here
 
     def test_post_map(self):
@@ -38,5 +35,5 @@ class MapTester(TestCase):
         Test saving the map to the server
         """
         # actually post test map data
-        response = self.client.post(reverse("gmap"))
-
+        response = self.client.post(reverse("gmap"), {"map_id": 1, "map_data": "{}"})
+        self.assertEquals(response.status_code, 200)

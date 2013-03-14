b640b7bbfb1cee1a7004d7767b08b6c0687088d3
Fangs <fangs@ubuntu.(none)>
2013-03-14 16:19:18 -0700

    testing

--- the_game_bazaar/tests.py
+++ the_game_bazaar/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from django.test.client import Client
+from django.utils import simplejson as json
+from the_game_bazaar import views
+from django.contrib.auth.models import User
 
 
 class SimpleTest(TestCase):
@@ -10,14 +13,12 @@ class SimpleTest(TestCase):
         self.assertEqual(1 + 1, 2)
 
 
-class Authtest(TestCase):
+class ourAuthTest(TestCase):
 	def test_ajax_login(self):
-        User.objects.create_user(username, email, password)
+        User.objects.create_user('aaa', 'aaa', 'aaa')
 		req = {
             'username':'aaa'
             'password':'aaa'
 		}
-
-	def test_ajax_register(self):
-
-	def test_ajax_logout(self):
+        s_resp = views.ajax_login(req)
+        self.assertEqual(json.loads(s_resp)['success'], True)

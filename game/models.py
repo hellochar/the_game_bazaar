1c09b1ca68d2550d8404f8866d47f1c4d1b01d92
James <jmuerle@jmuerle-ubuntu.(none)>
2013-03-15 14:15:49 -0700

    Testing json issues

--- game/models.py
+++ game/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from gmap.models import Map
 
+
 class Game(models.Model):
     # The map id of this game.
     map_id = models.ForeignKey(Map, related_name="+")
--- game/tests.py
+++ game/tests.py
@@ -1,16 +1,44 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from game import views
+from game.models import Game
+from gmap.models import Map
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 import json
 
-class UserListTests(TestCase):
+
+class MockRequest:
+    GET = {}
+
+
+class GameControllerTest(TestCase):
+    def setUp(self):
+        User.objects.create_user("username", "email", "password")
+        user = authenticate(username="username", password="password")
+        aMap = Map(creator_id=user, num_players=3, data="{}", map_name="Oogie Boogie")
+        aMap.save()
+
     def test_userlist(self):
-        """
-        Test user list to see if the correct response is returned
-        """
-        self.assertEqual(1 + 1, 2)
+        game = Game(map_id=Map.objects.all()[0], players='"{0: "player1", "1": "player2"}"')
+        game.save()
+        req = MockRequest()
+        req.GET = {
+            "game_id": game.id
+        }
+        resp = views.user_list(req)
+        data = json.loads(resp.content)['players']
+        data = json.loads(data)
+        for key, value in data.items():
+            print "Key is " + key
+            print "Value is " + value
+        self.assertTrue(0 in data)
+        self.assertTrue(1 in data)
+        self.assertEqual('player1', data[0])
+        self.assertEqual('player2', data[1])
+        pass
+
+    def test_hostgame(self):
+        pass
+
+    def test_joingame(self):
+        pass

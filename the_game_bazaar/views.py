89b837237a1637ee1c1b0370b1205679ad85d6b3
Fangs <fangs@ubuntu.(none)>
2013-03-20 15:37:58 -0700

    ordered games by id with higher id first in the list

--- the_game_bazaar/views.py
+++ the_game_bazaar/views.py
@@ -34,7 +34,7 @@ def home(request):
 def play(request):
     context = {
         "maps": Map.objects.all(),
-        "games": Game.objects.all(),
+        "games": Game.objects.order_by('id').reverse(),
         "lobby": Game.LOBBY,
     }
     return render(request, 'the_game_bazaar/play.html', context)

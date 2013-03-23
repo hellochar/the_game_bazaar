8efcfcb4e27aba08b85846506465d26d8cfcd2a9
hellochar <hellocharlien@hotmail.com>
2013-03-22 17:25:01 -0700

    Testing map editor

--- editor/static/editor/js/editor.js
+++ editor/static/editor/js/editor.js
@@ -12,37 +12,37 @@ function createMapFromResponse(response) {
     return map;
 }
 
-$(function() {
-    function setEditingMap(map) {
-        window.map = map;
-        $('#map-id')[0].innerHTML = map.id;
-        render();
-    }
-    function saveMap() {
-        data = {map_id : window.map.id, map_data : JSON.stringify(window.map.toJSON())};
-        $.post(
+function setEditingMap(map) {
+    window.map = map;
+    $('#map-id').text(map.id);
+    render();
+}
+
+function saveMap() {
+    data = {map_id : window.map.id, map_data : JSON.stringify(window.map.toJSON())};
+    $.post(
             '/map/',
             data,
             function(data, textStatus, jqXHR) {
                 console.log(data);
             },
             'json'
-            ).success(function (response_json) {
-                console.log("success", response_json);
-                window.map.id = response_json.map_id;
-                setEditingMap(window.map); //this call just updates the map-id element
-            }).done(function (resp) {
-                console.log('resp');
-                console.log(resp);
-            }).error(function (resp) {
-                console.log("errored");
-                console.log(resp);
-            });
-    }
+          ).success(function (response_json) {
+              console.log("success", response_json);
+              window.map.id = response_json.map_id;
+              setEditingMap(window.map); //this call just updates the map-id element
+          }).done(function (resp) {
+              console.log('resp');
+              console.log(resp);
+          }).error(function (resp) {
+              console.log("errored");
+              console.log(resp);
+          });
+}
 
-    //Load a map from the server with the given map id
-    function loadMap(map_id) {
-        $.getJSON( //Returned content will automatically be parsed into JSON
+//Load a map from the server with the given map id
+function loadMap(map_id) {
+    $.getJSON( //Returned content will automatically be parsed into JSON
             '/map',
             {map_id : map_id},
             function(data, textStatus, jqXHR) {
@@ -51,8 +51,33 @@ $(function() {
             ).success(function (data_json) {
                 setEditingMap( createMapFromResponse(data_json) );
             });
-    }
+}
 
+function currentPlayer() {
+    return window.map.players[$('input[name=player]:checked').val()];
+}
+
+function render() {
+    var canvas = $('#editor-canvas')[0];
+    var context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    window.map.players.forEach(function (player, idx) {
+        context.fillStyle = GameState.PLAYER_COLORS[idx];
+        player.units.forEach(function (unit) {
+            // context.fillRect(unit.init_pos.x - 10, unit.init_pos.y - 10, 20, 20);
+            context.beginPath();
+            context.arc(unit.pos(0).x, unit.pos(0).y, 10, 0, Math.PI*2, true);
+            context.closePath();
+            context.fill();
+        });
+    });
+}
+
+$(function() {
+    if(window.location.pathname != '/editor/') { //hack to make editor.js testable
+        return;
+    }
     if(window.location.hash != "") {
         //hash exists; load the map
         loadMap(window.location.hash.substring(1));
@@ -61,9 +86,6 @@ $(function() {
         setEditingMap(createDefaultMap());
     }
 
-    function currentPlayer() {
-        return window.map.players[$('input[name=player]:checked').val()];
-    }
 
     $('#editor-canvas').mousedown(function (evt) {
         var offsetX = $('#editor-canvas').position().left;
@@ -78,23 +100,5 @@ $(function() {
         loadMap($('#load-id').val());
     });
 
-    function render() {
-        var canvas = $('#editor-canvas')[0];
-        var context = canvas.getContext('2d');
-        context.clearRect(0, 0, canvas.width, canvas.height);
-
-        window.map.players.forEach(function (player, idx) {
-            context.fillStyle = GameState.PLAYER_COLORS[idx];
-            player.units.forEach(function (unit) {
-                // context.fillRect(unit.init_pos.x - 10, unit.init_pos.y - 10, 20, 20);
-                context.beginPath();
-                context.arc(unit.pos(0).x, unit.pos(0).y, 10, 0, Math.PI*2, true);
-                context.closePath();
-                context.fill();
-            });
-        });
-    }
-
 
 });
-
--- jasmine_testing/static/jasmine_testing/specs/EditorSpec.js
+++ jasmine_testing/static/jasmine_testing/specs/EditorSpec.js
@@ -1,85 +1,13 @@
 describe("Map Editor", function() {
+  beforeEach(function() {
+    //create fake ui elements
+    $('<div/>', {id: 'hidden-ui', style: 'display: none'}).appendTo('body');
 
-  describe("toJSON", function() {
-    // Instantiate a testing game state with three players and some amount of units for each.
-    var gamestate;
-    beforeEach(function() {
-      gamestate = new GameState([
-        new Player([
-          new Unit({'x': 0, 'y': 100}),
-          new Unit({'x': 1, 'y': 101})
-          ]),
-        new Player([
-          new Unit({'x': 2000, 'y': 2100})
-          ]),
-        new Player([
-          ])
-        ]);
-    });
-
-    describe("GameState.toJSON", function() {
-      var json;
-      beforeEach(function() {
-        json = gamestate.toJSON();
-      });
-
-      it("should only save the players attribute", function() {
-        expect(Object.keys(json)).toEqual(['players']);
-      });
-      it("should have the correct number of players", function() {
-        expect(json.players.length).toEqual(3);
-      });
-
-      // I would like to have test like this but it's impossible to test for "functional" equality in the pos() functions of units
-      // it("should be the inverse of fromJSON", function() {
-      //   expect(GameState.fromJSON(gamestate.toJSON())).toEqual(gamestate);
-      // });
-
-    });
-
-    describe("Player.toJSON", function() {
-      var json;
-      beforeEach(function() {
-        json = gamestate.players[0].toJSON();
-      });
-      it("should have the correct number of units", function() {
-        expect(Object.keys(json)).toEqual(['units']);
-      });
-      it("should only save the units attributes", function() {
-        expect(json.units.length).toEqual(2);
-      });
-    });
-
-    describe("Unit.toJSON", function() {
-      var json;
-      beforeEach(function() {
-        json = gamestate.players[0].units[0].toJSON();
-      });
-      it("should save the position and speed and nothing else", function() {
-        expect(json).toEqual({
-          init_pos: {
-            x: 0,
-          y: 100
-          },
-          speed: 0.1
-        });
-      });
-    });
-
+    $('<div/>', {id: 'map-id'}).appendTo('#hidden-ui');
+    $('<canvas/>', {id: 'editor-canvas'}).appendTo('#hidden-ui');
   });
-
-  describe("createMapFromResponse", function() {
-    it("should correctly set map_id", function() {
-      var response = {
-        map_id : 12,
-        success : true,
-        map_data : JSON.stringify({
-          players : []
-        })
-      };
-      var created_map = createMapFromResponse(response);
-      expect(created_map.id).toEqual(12);
-    });
+  afterEach(function() {
+    $('#hidden-ui').remove();
   });
 
   describe("createDefaultMap", function() {
@@ -95,14 +23,48 @@ describe("Map Editor", function() {
     });
   });
 
-  describe("Editor UI", function() {
-    describe("Adding a unit", function() {
-      it("should add a unit with correct player/pos attributes", function() {
-        var map = createDefaultMap();
-        map.addUnit(map.players[0], {x: 100, y: 200});
-        expect(map.players[0].units[0].pos(0)).toEqual({x:100, y:200});
-      });
+  describe("createMapFromResponse", function() {
+    it("should correctly set map_id", function() {
+      var response = {
+        map_id : 12,
+        success : true,
+        map_data : JSON.stringify(createDefaultMap().toJSON()),
+      };
+      var created_map = createMapFromResponse(response);
+      expect(created_map.id).toEqual(12);
     });
+  });
+
+  describe("setEditingMap", function() {
+    var map;
+    beforeEach(function() {
+      map = createDefaultMap();
+    });
+    it("should set the window.map variable and #map-id's text", function() {
+      setEditingMap(map);
+      expect(window.map).toEqual(map);
+      expect($('#map-id').text()).toEqual('');
+    });
+
+    it("should update #map-id text when it changes", function() {
+      map.id = 9;
+      setEditingMap(map);
+      expect($('#map-id').text()).toEqual('9');
+
+      var newMap = createDefaultMap();
+      newMap.id = 12;
+      setEditingMap(newMap);
+      expect($('#map-id').text()).toEqual('12');
+    });
+  });
+
+  describe("saveMap", function() {
+  });
+
+  describe("loadMap", function() {
+  });
+
+  describe("currentPlayer", function() {
   });
 
 });

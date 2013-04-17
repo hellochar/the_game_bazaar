1adf8cdc0d02ea079a1c107f48d4ab97f0c09c28
hellochar <hellocharlien@hotmail.com>
2013-04-17 16:50:02 -0700

    accidentally copied incorrect UnitPalette into its own file; fixed now

--- editor/static/editor/js/unit_palette.js
+++ editor/static/editor/js/unit_palette.js
@@ -1,5 +1,33 @@
 function UnitPalette(editor) {
     Palette.call(this, editor);
+
+    (function() {
+        var mouseDownButton = false; //one of: 1,2,3 or FALSE
+
+        var onMouseMove = function(evt) {
+            if(mouseDownButton === 1) {
+                this.tryAddUnit(editor.ui_renderer.getCanvasCoords(evt));
+            }
+        }.bind(this);
+
+        var onMouseDown = function(evt) {
+            mouseDownButton = evt.which;
+        }
+        var onMouseUp = function(evt) {
+            mouseDownButton = false;
+        }
+
+        $(this).bind("selectionGained", function(evt, oldPalette) {
+            $(this.editor.ui_renderer.canvas).on("mousedown", onMouseDown);
+            $(this.editor.ui_renderer.canvas).on("mousemove", onMouseMove);
+            $(this.editor.ui_renderer.canvas).on("mouseup", onMouseUp);
+        });
+        $(this).bind("selectionLost", function(evt, newPalette) {
+            $(this.editor.ui_renderer.canvas).off("mousedown", onMouseDown);
+            $(this.editor.ui_renderer.canvas).off("mousemove", onMouseMove);
+            $(this.editor.ui_renderer.canvas).off("mouseup", onMouseUp);
+        });
+    }.bind(this))();
 }
 
 UnitPalette.prototype = Object.create( Palette.prototype );
@@ -9,22 +37,26 @@ UnitPalette.domElement = (function() {
     var container = $('<div/>');
 
     $('<input/>', {type: 'radio', name: 'player', value: 0, checked: 'yes'}).appendTo(container);
-    container.append('Player 0');
-    container.append('<br/>');
+    $(container).append('Player 0');
+    $(container).append('<br/>');
 
     $('<input/>', {type: 'radio', name: 'player', value: 1}).appendTo(container);
-    container.append('Player 1');
-    container.append('<br/>');
+    $(container).append('Player 1');
+    $(container).append('<br/>');
 
     return container;
 })();
 
+UnitPalette.prototype.tryAddUnit = function(pos) {
+    var gamestate = this.editor.map.evaluate(0);
+    if(unitsInSphere(gamestate, pos, 30).length == 0) {
+        this.editor.map.addUnit(this.currentPlayer(), pos);
+    }
+}
+
 UnitPalette.prototype.handleClick = function(clicktype, clickpos) {
     if(clicktype == 1) {
-        var gamestate = this.editor.map.evaluate(0);
-        if(unitsInSphere(gamestate, clickpos, 30).length == 0) {
-            this.editor.map.addUnit(this.currentPlayer(), clickpos);
-        }
+        this.tryAddUnit(clickpos);
     }
 };
 UnitPalette.prototype.handleDrag = function(clicktype, dragstart, dragend) {
@@ -35,5 +67,3 @@ UnitPalette.prototype.currentPlayer = function() {
     var pid = $('input[name=player]:checked', Palette.domElement).val();
     return this.editor.map.players[pid];
 };
-
-

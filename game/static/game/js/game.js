61501e9dc8337b174a424195965280089cfb6c64
James <jmuerle@jmuerle-ubuntu.(none)>
2013-04-26 15:55:26 -0700

    Victorious and Defeat messages added to the game player

--- game/static/game/js/game.js
+++ game/static/game/js/game.js
@@ -140,9 +140,13 @@ Game.prototype.handleGameStart = function (data) {
 // If one of this player's units is expired, send the deadUnit message to the
 // server.
 Game.prototype.checkDeadUnits = function(game_time) {
+    // Check if we're okay to send dead units again.
+    if (this.waitingForDeadUnits) {
+        return;
+    }
     // Detect bullet collisions and send messages to the server if appropriate.
     var deadUnitIndexList = [];
-    this.gamestate.players[self.player_id].units.forEach(function(unit, index) {
+    this.gamestate.players[this.player_id].units.forEach(function(unit, index) {
         if (unit.deadTime && unit.deadTime <= (game_time)) {
             deadUnitIndexList.push(index);
         }
@@ -153,7 +157,8 @@ Game.prototype.checkDeadUnits = function(game_time) {
             'player_id': this.player_id,
             'deadUnitIndexList': deadUnitIndexList
         };
-        self.socket.emit('deadUnits', data);
+        this.socket.emit('deadUnits', data);
+        this.waitingForDeadUnits = true;
     }
 };
 
@@ -184,42 +189,41 @@ Game.prototype.checkWinAndLose = function() {
 };
 
 Game.prototype.renderMethod = function() {
-    var self = this;
     var now_time = Date.now();
-    var start_time = self.client_start_time;
+    var start_time = this.client_start_time;
 
-    self.checkDeadUnits(now_time - start_time);
-    self.checkWinAndLose();
+    this.checkDeadUnits(now_time - start_time);
+    this.checkWinAndLose();
 
     // Render this snapshot of the gamestate.
-    var snapshot = self.gamestate.evaluate(now_time - start_time);
+    var snapshot = this.gamestate.evaluate(now_time - start_time);
 
     var renderText = function(text) {
-        self.ui_renderer.renderText(text, 400, 200, "red");
+        this.ui_renderer.renderText(text, 400, 200, "red");
     };
-    self.gs_renderer.update(snapshot);
-    self.gs_renderer.animate();
-    switch (self.conn_state) {
+    this.gs_renderer.update(snapshot);
+    this.gs_renderer.animate();
+    switch (this.conn_state) {
         case Game.GAME_STATES.CONNECTED:
-            self.ui_renderer.renderSelectRect();
+            this.ui_renderer.renderSelectRect();
             var d1 = new THREE.Vector3(0, 0, 0);
             var d2 = new THREE.Vector3(window.innerWidth, 0, 0);
             var d3 = new THREE.Vector3(window.innerWidth, window.innerHeight, 0);
             var d4 = new THREE.Vector3(0, window.innerHeight, 0);
-            d1 = self.gs_renderer.project(d1);
-            d2 = self.gs_renderer.project(d2);
-            d3 = self.gs_renderer.project(d3);
-            d4 = self.gs_renderer.project(d4);
+            d1 = this.gs_renderer.project(d1);
+            d2 = this.gs_renderer.project(d2);
+            d3 = this.gs_renderer.project(d3);
+            d4 = this.gs_renderer.project(d4);
 
-            // self.ui_renderer.renderText("d1 x: " + d1.x + ", y: " + d1.y, 400, 200, "red");
-            // self.ui_renderer.renderText("d2 x: " + d2.x + ", y: " + d2.y, 400, 220, "red");
-            // self.ui_renderer.renderText("d3 x: " + d3.x + ", y: " + d3.y, 400, 240, "red");
-            // self.ui_renderer.renderText("d4 x: " + d4.x + ", y: " + d4.y, 400, 260, "red");
+            // this.ui_renderer.renderText("d1 x: " + d1.x + ", y: " + d1.y, 400, 200, "red");
+            // this.ui_renderer.renderText("d2 x: " + d2.x + ", y: " + d2.y, 400, 220, "red");
+            // this.ui_renderer.renderText("d3 x: " + d3.x + ", y: " + d3.y, 400, 240, "red");
+            // this.ui_renderer.renderText("d4 x: " + d4.x + ", y: " + d4.y, 400, 260, "red");
 
-            self.ui_renderer.renderMap();
-            self.ui_renderer.renderViewPort(d1, d2, d3, d4);
-            self.ui_renderer.renderGS(snapshot);
-            self.ui_renderer.renderSelectionCircles(snapshot.players[self.player_id].selectedUnits);
+            this.ui_renderer.renderMap();
+            this.ui_renderer.renderViewPort(d1, d2, d3, d4);
+            this.ui_renderer.renderGS(snapshot);
+            this.ui_renderer.renderSelectionCircles(snapshot.players[this.player_id].selectedUnits);
 
             var delta = new THREE.Vector3(0, 0, 0);
             if (this.keys.w) {
@@ -234,8 +238,8 @@ Game.prototype.renderMethod = function() {
             if (this.keys.d) {
                 delta.x += 100;
             }
-            var pos = self.gs_renderer.getViewport();
-            self.gs_renderer.setViewport(pos.x + delta.x, pos.y + delta.y);
+            var pos = this.gs_renderer.getViewport();
+            this.gs_renderer.setViewport(pos.x + delta.x, pos.y + delta.y);
             break;
         case Game.GAME_STATES.INIT:
             renderText("Initializing...");
@@ -507,6 +511,11 @@ Game.prototype.handleDeadUnits = function(data) {
     var player_id = data['player_id'];
     var deadUnitIndexList = data['deadUnitIndexList'];
 
+    // If we are the player whose units are dying, set waiting to false.
+    if (player_id === this.player_id) {
+        this.waitingForDeadUnits = false;
+    }
+
     var player = this.gamestate.players[player_id];
     player.units = player.units.filter(function(unit, index) {
         var alive = deadUnitIndexList.indexOf(index) === -1;
@@ -517,11 +526,13 @@ Game.prototype.handleDeadUnits = function(data) {
                 bullet.collidedUnit.killingBullet = false;
                 bullet.collidedUnit = false;
             });
+
             // Remove the other bullet from the gamestate.
-            var others_list = unit.killingBullet.unit.bullets;
-            unit.killingBullet.unit.bullets = others_list.filter(function(other_bullet) {
+            var others_list = unit.killingBullet.collidedUnit.bullets;
+            unit.killingBullet.collidedUnit.bullets = others_list.filter(function(other_bullet) {
                 return other_bullet !== unit.killingBullet;
             });
+
             // Remove the dead unit from the selected units
             player.selectedUnits = player.selectedUnits.filter(function(selected_unit) {
                 return selected_unit !== unit;
@@ -556,7 +567,9 @@ Game.prototype.handleLostGame = function(data) {
     // TODO
     this.gamestate.players[player_id].lost = true;
     // DEBUG
-    console.log("Player " + player_id.toString() + "has lost.");
+    if (player_id === this.player_id) {
+        this.ui_renderer.lost = true;
+    }
 };
 
 Game.prototype.handleWonGame = function(data) {
@@ -565,7 +578,9 @@ Game.prototype.handleWonGame = function(data) {
     // TODO
     this.gamestate.players[player_id].won = true;
     // DEBUG
-    console.log("Player " + player_id.toString() + "has won.");
+    if (player_id === this.player_id) {
+        this.ui_renderer.won = true;
+    }
 };
 
 

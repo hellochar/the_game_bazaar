606bcf45b5441f8fa9c16415fcc9af7ce499ecfd
hellochar <hellocharlien@hotmail.com>
2013-04-29 19:18:07 -0700

    Palettes can now use all of ui_renderer input events; obstacles render as you're drawing them; add dragMove handler; add documentation for state in ui_renderer

--- editor/static/editor/js/editor.js
+++ editor/static/editor/js/editor.js
@@ -8,9 +8,21 @@ function Editor(map, ui_renderer) {
     this.setEditingMap(map || Editor.createDefaultMap());
 }
 
+// Should bind all input events from the UI renderer to the Editor
 Editor.prototype.init = function() {
-    this.ui_renderer.bindClick(this.handleClick.bind(this));
-    this.ui_renderer.bindDragEnd(this.handleDragEnd.bind(this));
+    // An array of all ui_renderer method names that start with 'bind'
+    var inputEventNames = Object.keys(UIRenderer.prototype).filter(function (name) { return name.match(/bind.*/); });
+    inputEventNames.forEach(function (methodName) {
+
+        // for each input that can be bound (e.g. bindKeyDown), delegate
+        // it to the currently active palette.
+        var handlerName = methodName.replace("bind", "handle");
+        this.ui_renderer[methodName](function () {
+            if( this.palette[handlerName] ) { //if the palette has defined the function, invoke it
+                this.palette[handlerName].apply(this.palette, arguments);
+            }
+        }.bind(this));
+    }.bind(this));
 
     this.ui_renderer.startRendering(this.renderMethod.bind(this));
 };
@@ -40,14 +52,6 @@ Editor.prototype.setPalette = function(palette) {
     $(this.palette).trigger("selectionGained", oldPalette);
     console.log("Set palette");
 }
-
-Editor.prototype.handleClick = function(clicktype, clickpos) {
-    this.palette.handleClick(clicktype, clickpos);
-};
-
-Editor.prototype.handleDragEnd = function(clicktype, dragstart, dragend) {
-    this.palette.handleDragEnd(clicktype, dragstart, dragend);
-};
 
 Editor.prototype.renderMethod = function() {
     var gamestate = this.map.evaluate(0);
--- editor/static/editor/js/obstacle_palette.js
+++ editor/static/editor/js/obstacle_palette.js
@@ -3,7 +3,7 @@ function ObstaclePalette(editor) {
     this.domElement = (function() {
         var container = $('<div/>');
 
-        container.append('Drag to create obstacles.');
+        container.append('Drag to create obstacles.<br>Press escape while dragging to cancel.</br>');
 
         return container;
     })();
@@ -15,11 +15,29 @@ ObstaclePalette.prototype.constructor = ObstaclePalette;
 
 ObstaclePalette.prototype.handleClick = function(clicktype, clickpos) {
 };
-ObstaclePalette.prototype.handleDragEnd = function(clicktype, dragstart, dragend) {
+
+ObstaclePalette.prototype.handleKeyUp = function(key) {
+    if(key == 27) { //escape
+        this.nodes = undefined;
+    }
+}
+
+ObstaclePalette.prototype.handleDragMove = function(clicktype, dragstart, dragend) {
     if(clicktype == 1) {
-        this.editor.map.addWall(dragstart, dragend);
+        if( ! this.nodes ) {
+            this.nodes = this.editor.map.addWall(dragstart, dragend);
+        } else {
+            this.nodes[1].pos = dragend;
+        }
     }
 };
+
+ObstaclePalette.prototype.handleDragEnd = function(clicktype, dragstart, dragend) {
+    if(clicktype == 1) {
+        this.nodes = undefined;
+    }
+};
+
 ObstaclePalette.prototype.renderMethod = function() {
 };
 
